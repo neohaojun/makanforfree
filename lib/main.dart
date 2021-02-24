4bf39b7415db27abc3faf7531cb2b6a2c4dfe1b2
neohaojun <neohaojun@gmail.com>
2021-02-24 18:12:04 +0800

    resolved undefined type "User" error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:makanforfree/screens/wrapper.dart';
 import 'package:makanforfree/services/auth.dart';
-import 'package:provider/provider.dart'; 
+import 'package:provider/provider.dart';
 import 'package:makanforfree/models/user.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +9,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
+    return StreamProvider<AppUser>.value(
       value: AuthService().user,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
